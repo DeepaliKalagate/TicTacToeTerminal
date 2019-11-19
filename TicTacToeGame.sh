7b768bae9f4575000e9f12f80cb798b9f3086659
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-19 11:58:51 +0530

    Add : Assigned Symbol to Player and Computer

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -1,8 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Tic Tac Toe Game"
 
 #Constant Variable
 MAX_POSITION=9
+
+#variables
+playerSymbol=''
+computerSymbol=''
 
 #Array
 declare -a boardPosition
@@ -15,4 +19,21 @@ function freshBoard()
 	done
 }
 
+function symbolAssignment()
+{
+	firstPlay=$((RANDOM%2))
+
+	if [ $firstPlay -eq 1 ]
+	then
+		playerSymbol='X'
+		computerSymbol='O'
+		echo "Player symobol : X | Computer symbol : O"
+	else
+		 playerSymbol='O'
+		 computerSymbol='X'
+		echo "Player symobol : O | Computer symbol : X"
+	fi
+}
+
 freshBoard
+symbolAssignment
