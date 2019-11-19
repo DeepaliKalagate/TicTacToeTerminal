325b22bde7415d972f19596c02fc3014ba99297c
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-19 11:49:35 +0530

    Initialized Empty Borad to Start Game

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 echo "Welcome to Tic Tac Toe Game"
 
+#Constant Variable
+MAX_POSITION=9
+
+#Array
+declare -a boardPosition
+
+function freshBoard()
+{
+	for (( i=1; i<=$MAX_POSITION; i++ ))
+	do
+		boardPosition[$i]=$i
+	done
+}
+
+freshBoard
